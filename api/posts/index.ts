0df6e094304bb23e36d533c065fbe9fd9c7b2fcf
Yukai Huang <yukaihuangtw@gmail.com>
2022-03-20 09:53:30 +0800

    style: deno fmt

--- api/posts/index.ts
+++ api/posts/index.ts
@@ -1,15 +1,15 @@
-import type { APIHandler } from 'aleph/types.d.ts'
-import { getQueryBuilder } from '~/lib/supabase.ts'
+import type { APIHandler } from 'aleph/types.d.ts';
+import { getQueryBuilder } from '~/lib/supabase.ts';
 
 export const handler: APIHandler = async ({ response }) => {
-  let { data: posts, error } = await getQueryBuilder('posts').select('*')
+  const { data: posts, error } = await getQueryBuilder('posts').select('*');
 
   if (error) {
-    response.status = 500
+    response.status = 500;
     response.json({
-      error
-    })
+      error,
+    });
   } else {
-    response.json({ posts })
+    response.json({ posts });
   }
-}
+};
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,13 @@
-import { createClient } from 'supabase'
-import { PostgrestQueryBuilder } from '@supabase/postgrest-js'
+import { createClient } from 'supabase';
+import { PostgrestQueryBuilder } from '@supabase/postgrest-js';
 
-const supabaseUrl = Deno.env.get('SUPABASE_URL') as string
-const supabaseKey = Deno.env.get('SUPABASE_KEY') as string
+const supabaseUrl = Deno.env.get('SUPABASE_URL') as string;
+const supabaseKey = Deno.env.get('SUPABASE_KEY') as string;
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
 
-export function getQueryBuilder<T> (tableName: string) {
-  return supabase.from(tableName) as unknown as PostgrestQueryBuilder<T>
+export function getQueryBuilder<T>(tableName: string) {
+  return supabase.from(tableName) as unknown as PostgrestQueryBuilder<T>;
 }
