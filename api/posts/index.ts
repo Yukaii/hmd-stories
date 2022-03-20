49d7f36624dc47c0ad3b68817db555cc48520113
Yukai Huang <yukaihuangtw@gmail.com>
2022-03-20 12:02:43 +0800

    feat: create post api

--- api/posts/index.ts
+++ api/posts/index.ts
@@ -1,7 +1,8 @@
 import type { APIHandler } from 'aleph/types.d.ts';
 import { getQueryBuilder } from '~/lib/supabase.ts';
+import { CreatePostPayload } from '~/types/index.ts'
 
-export const handler: APIHandler = async ({ response }) => {
+const getPosts: APIHandler = async ({ response }) => {
   const { data: posts, error } = await getQueryBuilder('posts').select('*');
 
   if (error) {
@@ -11,5 +12,46 @@ export const handler: APIHandler = async ({ response }) => {
     });
   } else {
     response.json({ posts });
+  }
+}
+
+const createPost: APIHandler = async ({ request, response }) => {
+  const body: CreatePostPayload = await request.json()
+
+  console.log(body)
+
+  const { userpath, content, variant } = body;
+
+  const { data } = await getQueryBuilder('posts').insert({
+    userpath,
+    content,
+    variant 
+  })
+
+  if (data) {
+    response.json({ post: data[0] });
+  } else {
+    response.status = 500;
+    response.json({
+      error: 'Error creating post',
+    });
+  }
+}
+
+export const handler: APIHandler = async (context) => {
+  const { response, request } = context;
+
+  switch (request.method) {
+    case 'GET': {
+      await getPosts(context);
+    }
+
+    case 'POST': {
+      console.log('shit')
+      await createPost(context);
+    }
+
+    default:
+      response.status = 405;
   }
 };
---
+++ types/index.ts
@@ -0,0 +1,30 @@
+export enum PostType {
+	BLACK,
+	GREY,
+	ORANGE,
+	PINK_AND_ORANGE,
+	PURPLE,
+	GREEN,
+	PURPLE_AND_PINK,
+	// RAINBOW
+}
+
+export type Post = {
+  id: string // UUID
+	userpath: string
+	content: string
+	variant: PostType,
+	deleteToken: string
+}
+
+export type CreatePostPayload = Pick<Post, 'userpath' | 'content' | 'variant'>
+
+export type CreatePost = Post & {
+	deleteToken: string
+}
+
+export type PostList = Post[]
+
+export type DeletePostPayload = {
+	deleteToken: string
+}
