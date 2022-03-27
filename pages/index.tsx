a91354a8f23f98746a7950ae9351fa0825d48a1b
Yukai Huang <yukaihuangtw@gmail.com>
2022-03-27 16:07:07 +0800

    feat: tune ellipses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 import Logo from '~/components/Logo.tsx';
 
 const Navbar = () => {
@@ -30,23 +31,45 @@ const textClipStyle = {
   WebkitTextFillColor: 'transparent',
 };
 
-const BackgroundEllipseOne = (style: React.StyleHTMLAttributes<HTMLDivElement>) => {
-  return <div style={{
-    background: 'radial-gradient(50% 50% at 50% 50%, rgba(27, 63, 156, 0.56) 0%, rgba(27, 63, 156, 0) 100%)',
-    opacity: 0.91,
-    ...style
-  }}>
-  </div>
-}
+const BackgroundEllipseOne = ({
+  style,
+  className,
+}: {
+  style?: React.StyleHTMLAttributes<HTMLDivElement>['style'];
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cx('block', className)}
+      style={{
+        background:
+          'radial-gradient(50% 50% at 50% 50%, rgba(27, 63, 156, 0.56) 0%, rgba(27, 63, 156, 0) 100%)',
+        opacity: 0.91,
+        ...style,
+      }}
+    ></div>
+  );
+};
 
-const BackgroundEllipseTwo = (style: React.StyleHTMLAttributes<HTMLDivElement>) => {
-  return <div style={{
-    background: 'radial-gradient(50% 50% at 50% 50%, rgba(156, 27, 58, 0.56) 0%, rgba(142, 21, 101, 0) 100%)',
-    opacity: 0.56,
-    ...style
-  }}>
-  </div>
-}
+const BackgroundEllipseTwo = ({
+  style,
+  className,
+}: {
+  style: React.StyleHTMLAttributes<HTMLDivElement>['style'];
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cx('block', className)}
+      style={{
+        background:
+          'radial-gradient(50% 50% at 50% 50%, rgba(156, 27, 58, 0.56) 0%, rgba(142, 21, 101, 0) 100%)',
+        opacity: 0.56,
+        ...style,
+      }}
+    ></div>
+  );
+};
 
 export default function Home() {
   return (
@@ -55,9 +78,22 @@ export default function Home() {
 
       <section
         className="relative flex flex-col items-center pt-24 origin-center"
-        style={{ height: 'calc(100vh - 60px)', backgroundImage: 'url(/images/bg-1.png)' }}
+        style={{
+          height: 'calc(100vh - 60px)',
+          backgroundImage: 'url(/images/bg-1.png)',
+        }}
       >
-        <div className="flex flex-col items-center justify-center">
+        <BackgroundEllipseOne
+          style={{ width: '40vw', height: '60vh', transform: 'translate(-80%, -10%)' }}
+          className="absolute left"
+        />
+
+        <BackgroundEllipseTwo
+          style={{ width: '60vw', height: '60vh', transform: 'translate(60%, -30%)' }}
+          className="absolute left"
+        />
+
+        <div className="z-10 flex flex-col items-center justify-center">
           <Logo className="text-2xl" />
 
           <h1 className="select-none font-sourceSans text-[10vw] sm:text-[10vw] md:text-7xl lg:text-8xl mt-4.5 mb-1">
@@ -65,7 +101,7 @@ export default function Home() {
             <span
               className="bg-clip-text"
               style={{
-                ...textClipStyle
+                ...textClipStyle,
               }}
             >
               MD Stories
@@ -76,13 +112,16 @@ export default function Home() {
             這是一段神奇的文字
           </h3>
 
-          <a style={textClipStyle} className='mt-12 mb-20'>
+          <a style={textClipStyle} className="mt-12 mb-20">
             霹靂卡霹靂拉拉波波力那貝貝魯多
-            <i className='fa fa-angle-right ml-0.5' aria-hidden='true' />
+            <i className="fa fa-angle-right ml-0.5" aria-hidden="true" />
           </a>
         </div>
 
-        <img className="w-full h-auto max-w-screen-md" src="/images/screenshot.png" />
+        <img
+          className="z-10 w-full h-auto max-w-screen-md"
+          src="/images/screenshot.png"
+        />
       </section>
     </div>
   );
